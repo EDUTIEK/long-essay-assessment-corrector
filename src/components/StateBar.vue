1b370239edb92914b5858c5d198c1eff9f290a27
Fred Neumann <fred.neumann@fau.de>
2023-02-10 10:14:28 +0100

    added pilot info

--- src/components/StateBar.vue
+++ src/components/StateBar.vue
@@ -5,6 +5,6 @@
 
 <template>
   <v-app-bar position="bottom" height="48" color="grey-lighten-5" elevation="1">
-    Letzte Änderung: {{summaryStore.openSending ? 'nicht gesendet' : 'gesendet'}}
+    Pilot (alpha) | Letzte Änderung: {{summaryStore.openSending ? 'nicht gesendet' : 'gesendet'}}
   </v-app-bar>
 </template>
