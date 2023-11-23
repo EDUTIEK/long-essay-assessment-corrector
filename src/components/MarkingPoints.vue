c16ebf34262f16edfbde80470f1479b2eccf72e5
Fred Neumann <fred.neumann@fau.de>
2023-11-23 16:05:33 +0100

    fix marking comments and points for two correctors

--- src/components/MarkingPoints.vue
+++ src/components/MarkingPoints.vue
@@ -4,7 +4,7 @@ import {useCommentsStore} from "@/store/comments";
 import {usePointsStore} from "@/store/points";
 import {useApiStore} from "@/store/api";
 import {useSummariesStore} from '@/store/summaries';
-import { ref, watch} from 'vue';
+import { ref, watch, nextTick} from 'vue';
 import TextMarker from '@/lib/TextMarker';
 
 const criteriaStore = useCriteriaStore();
@@ -14,37 +14,22 @@ const apiStore = useApiStore();
 const summariesStore = useSummariesStore();
 
 let corrector_key = ref('');
+let criteriaPoints = ref({});
 
-function loadPoints() {
+async function loadPoints() {
     const comment = commentsStore.getComment(commentsStore.selectedKey);
-    if (comment) {
-      corrector_key.value = comment.corrector_key;
-    }
-    else {
-      corrector_key.value = '';
-    }
+    corrector_key.value = comment ? comment.corrector_key :  '';
 
-    criteriaStore.getCorrectorCriteria(corrector_key.value).forEach(criterion => {
-      let value = pointsStore.getValueByRelation(commentsStore.selectedKey, criterion.key);
-      let el = document.getElementById('pointsInput' + criterion.key);
-      if (el !== undefined && el !== null) {
-        el.value = value;
-        if (corrector_key.value == '' || corrector_key.value != apiStore.correctorKey) {
-          el.setAttribute('disabled', 'disabled');
-        }
-        else {
-          el.removeAttribute('disabled');
-        }
-      }
-   });
+    criteriaPoints.value = {};
+    for (const criterion of criteriaStore.getCorrectorCriteria(corrector_key.value)) {
+      criteriaPoints.value[criterion.key] = pointsStore.getValueByRelation(commentsStore.selectedKey, criterion.key);
+    }
 }
 watch(() => commentsStore.selectedKey, loadPoints);
 
+
 function savePoints(criterionKey) {
-    let commentKey = commentsStore.selectedKey;
-    let el = document.getElementById('pointsInput' + criterionKey);
-    let value = el.value;
-    pointsStore.setValueByRelation(commentKey, criterionKey, value);
+    pointsStore.setValueByRelation(commentKey, criterionKey, criteriaPoints.value[criterionKey]);
 }
 
 </script>
@@ -70,11 +55,11 @@ function savePoints(criterionKey) {
                         <label :for="'pointsInput' + criterion.key">{{ criterion.title}}</label>
                     </td>
                     <td  class="text-right">
-                        <input class="appPoints" type="number" min="0"
-                               :disabled="summariesStore.isOwnDisabled"
-                               :id="'pointsInput' + criterion.key"
+                        <input class="appPoints" type="number" min="0" v-model="criteriaPoints[criterion.key]"
+                               :disabled="summariesStore.isOwnDisabled || corrector_key != apiStore.correctorKey"
                                :max="criterion.points"
-                               @change="savePoints(criterion.key)" /> / {{criterion.points}}
+                               @change="savePoints(criterion.key)" 
+                        /> / {{criterion.points}}
                     </td>
                 </tr>
             </tbody>
